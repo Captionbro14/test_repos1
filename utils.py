bad9997eb399d7fde3063923d8d01610772386f1
kevinnadar <jesikamaraj@gmail.com>
2022-08-20 12:01:45 +0530

    .

--- utils.py
+++ utils.py
@@ -380,8 +380,7 @@ shortz = shortzy.Shortzy("shareus.in", SHORTENER_API)
 async def get_shortlink(link):
     if SHORTENER_API:
         if LONG_DROPLINK_URL == "True" or LONG_DROPLINK_URL is True:
-            return await shortz.get_quick_link(link, silently_fail=True)
+            return await shortz.get_quick_link(link)
         else:
-            return await shortz.convert(link, silently_fail=True)
-
+            return await shortz.convert(link, silently_fail=False)
     return link
