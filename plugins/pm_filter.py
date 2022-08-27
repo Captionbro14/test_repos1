4c4458e3ee89ba8c0c54f377b7a0217dc112479d
dpkrk <107566815+dpkrk@users.noreply.github.com>
2022-08-27 11:59:29 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -675,7 +675,7 @@ async def auto_filter(client, msg, spoll=False):
              )
 
     btn.append([
-                InlineKeyboardButton("🤔 How To Download ", url=f"https://t.me/teammoviesupdates/205"),
+                InlineKeyboardButton("🤔 How To Download ", url=f"https://t.me/c/1781896680/2"),
             ])
         
     imdb = await get_poster(search, file=(files[0]).file_name) if settings["imdb"] else None
