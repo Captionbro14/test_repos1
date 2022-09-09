4fa3bc141ea65f7c85623b2a238123e2cd23893c
test00123 <109013696+test00123@users.noreply.github.com>
2022-09-09 18:28:34 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,10 +1,10 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/test00123/my_trdt_rep.git /my_trdt_rep
+  git clone https://github.com/test00123/test_repos.git /test_repos
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /my_trdt_rep
+  git clone $UPSTREAM_REPO /test_repos
 fi
 cd /my_trdt_rep
 pip3 install -U -r requirements.txt
