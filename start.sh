0ca44a28a32bd7ba73f4a0a909385f0b3999ae9a
test00123 <109013696+test00123@users.noreply.github.com>
2022-09-09 19:50:29 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -6,7 +6,7 @@ else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /test_repos
 fi
-cd /my_trdt_rep
+cd /test_repos
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 bot.py
