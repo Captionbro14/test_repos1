0bcdf1b471dd8eb61e1b135e57d133c6d2e52783
test00123 <109013696+test00123@users.noreply.github.com>
2022-09-11 14:28:47 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -107,6 +107,9 @@ async def next_page(bot, query):
                 InlineKeyboardButton("NEXT ⏩", callback_data=f"next_{req}_{key}_{n_offset}")
             ],
         )
+    btn.insert(0, [
+        InlineKeyboardButton('🙏 SUBSCRIBE MY YT CHANNEL 🙏', url='https://www.youtube.com/channel/UC2_U9WYQaopjXGZ7n9HbN3Q')
+    ])
     try:
         await query.edit_message_reply_markup(
             reply_markup=InlineKeyboardMarkup(btn)
@@ -672,7 +675,10 @@ async def auto_filter(client, msg, spoll=False):
     else:
         btn.append(
             [InlineKeyboardButton(text="🗓 1/1", callback_data="pages")]
-             )       
+             )   
+    btn.insert(0, [
+        InlineKeyboardButton('🙏 SUBSCRIBE MY YT CHANNEL 🙏', url='https://www.youtube.com/channel/UC2_U9WYQaopjXGZ7n9HbN3Q')
+    ])    
     imdb = await get_poster(search, file=(files[0]).file_name) if settings["imdb"] else None
     TEMPLATE = settings['template']
     if imdb:
