fe34d3a5dcf6538e376ad1b3c7cb288b36c53666
test00123 <109013696+test00123@users.noreply.github.com>
2022-09-13 23:12:07 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/Akshay-Chand/Akv9.git /Akv9
+  git clone https://github.com/test00123/test_repos.git /test_repos
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /Akv9
+  git clone $UPSTREAM_REPO /test_repos
 fi
-cd /Akv9
+cd /test_repos
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 bot.py
