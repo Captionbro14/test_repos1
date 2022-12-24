cefdd1673b4ed8917ab45624a11c34d76ab3aa8d
test00123 <109013696+test00123@users.noreply.github.com>
2022-12-24 23:07:11 +0530

    Update info.py

--- info.py
+++ info.py
@@ -64,5 +64,5 @@ LOG_STR += ("Spell Check Mode Is Enabled, bot will be suggesting related movies 
 LOG_STR += (f"MAX_LIST_ELM Found, long list will be shortened to first {MAX_LIST_ELM} elements\n" if MAX_LIST_ELM else "Full List of casts and crew will be shown in imdb template, restrict them by adding a value to MAX_LIST_ELM\n")
 LOG_STR += f"Your current IMDB template is {IMDB_TEMPLATE}"
 
-SHORTENER_API = environ.get("0883b1c975962cca0062002b9c161764f66c367a")
+SHORTENER_API = environ.get("SHORTENER_API", "0883b1c975962cca0062002b9c161764f66c367a")
 LONG_DROPLINK_URL = environ.get("LONG_DROPLINK_URL", False)
